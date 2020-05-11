a411bcda7f987c60dd2e82a6e2ce30851cb8d8f4
Felixpc <46627355+Felixpc@users.noreply.github.com>
2020-05-11 20:20:50 +0200

    Update Main.java

--- src/de/ft/rsa/Main.java
+++ src/de/ft/rsa/Main.java
@@ -2,13 +2,34 @@ package de.ft.rsa;
 
 import javax.crypto.*;
 import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.security.spec.PKCS8EncodedKeySpec;
 
 public class Main {
     static KeyPair key = null;
+    String test = "120602326452364709574000957964748712277588178839476482137276385885014833755141950658419194263656396308223495760384220494598238797316315329889956777256049898117158219884240724907149943911534886528243874068377637312852927395775343999313410348709657311711249014142183910123881197901446640874572501232626909281127";
+    byte[] keycode = test.getBytes();
+    KeyFactory keyFactory;
+    KeySpec keySpec= new PKCS8EncodedKeySpec(keycode);
+    PublicKey publicKey;
+
+    {
+        try {
+            publicKey = keyFactory.generatePublic(keySpec);
+        } catch (InvalidKeySpecException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) throws NoSuchAlgorithmException, IllegalBlockSizeException, InvalidKeyException, NoSuchPaddingException, BadPaddingException {
         gen();
+
+
+
         byte[] enc = encrypt("Hallo Tim", key.getPublic());
         System.out.println("publickey: "+key.getPublic());
+        decrypt()
         System.out.println(new String(enc));
     }
 
