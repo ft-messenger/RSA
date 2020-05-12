56b7ab7cef0a1d04498c1b86396304ad168aa531
Coder246 <tim-arnold@outlook.de>
2020-05-12 15:39:13 +0200

    update

--- src/de/ft/rsa/Main.java
+++ src/de/ft/rsa/Main.java
@@ -1,13 +1,34 @@
 package de.ft.rsa;
 
 import javax.crypto.*;
+import java.math.BigInteger;
 import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.RSAPublicKeySpec;
 
 public class Main {
     static KeyPair key = null;
+    KeySpec keySpec;
+    static PublicKey publicKey;
+
+
+
+
     public static void main(String[] args) throws NoSuchAlgorithmException, IllegalBlockSizeException, InvalidKeyException, NoSuchPaddingException, BadPaddingException {
+
+
+        try {
+            publicKey = KeyFactory.getInstance("RSA").generatePublic(new RSAPublicKeySpec( new BigInteger("10910853303221765486890206096462437523034786405640349999702259107045652321478800202054466683468758145555104815413076674318387553951639597769938150328850093"), new BigInteger("65537")));
+        } catch (InvalidKeySpecException e) {
+            e.printStackTrace();
+        }
+
+
         gen();
-        byte[] enc = encrypt("Hallo Tim", key.getPublic());
+        byte[] bytetest = new String("test").getBytes();
+        byte[] enc = encrypt("bytetest", publicKey);
         System.out.println("publickey: "+key.getPublic());
         System.out.println(new String(enc));
     }
